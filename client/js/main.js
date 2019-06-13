9647d194bfd52eab66d8ac66686fe1e2a701ddad
Kevin Weiss <kev_asher@hotmail.com>
2019-06-13 10:37:30 -0300

    fix stun urls

--- client/js/main.js
+++ client/js/main.js
@@ -6,11 +6,11 @@ var socket = io();
 const servers = { 
   iceServers: [{ 
     urls: [
-      'stun.l.google.com',
-      'stun1.l.google.com',
-      'stun2.l.google.com',
-      'stun3.l.google.com',
-      'stun4.l.google.com',
+      'stun:stun.l.google.com',
+      'stun:stun1.l.google.com',
+      'stun:stun2.l.google.com',
+      'stun:stun3.l.google.com',
+      'stun:stun4.l.google.com',
     ] 
   }] 
 };
