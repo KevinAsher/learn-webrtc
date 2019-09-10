3c203a747ee967ad89b8adb2fe41644e69aa5080
KevinAsher <kev_asher@hotmail.com>
2019-09-10 13:45:14 -0300

    Fix eslint warnings, and fix STUN server ports

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,6 @@
 'use strict';
+/* global io, $ */
+
 
 var socket = io();
 
@@ -12,11 +14,11 @@ const servers = {
   },
   { 
     urls: [
-      'stun:stun.l.google.com',
-      'stun:stun1.l.google.com',
-      'stun:stun2.l.google.com',
-      'stun:stun3.l.google.com',
-      'stun:stun4.l.google.com',
+      'stun:stun.l.google.com:19302',
+      'stun:stun1.l.google.com:19302',
+      'stun:stun2.l.google.com:19302',
+      'stun:stun3.l.google.com:19302',
+      'stun:stun4.l.google.com:19302',
     ] 
   }] 
 };
