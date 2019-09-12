97d3c81b79e215a5595f0968955a387c6b651e94
Kevin Weiss <kev_asher@hotmail.com>
2019-09-12 12:53:19 -0300

    adiciona ao socket.io events para tratar entrada/saida dos peers as salas

--- index.js
+++ index.js
@@ -12,4 +12,22 @@ http.listen(port, function(){
 });
 
 // Insira código do socket.io aqui
+io.on('connection', function (socket) {
+  console.log('user connected #' + socket.id);
 
+  // somebody closed the browser tab
+  socket.on('disconnect', function () {
+    console.log('somebody disconnected');
+  });
+
+  socket.on('join-room', function (room) {
+    socket.join(room);
+  });
+
+  socket.on('leave-room', function (room) {
+    socket.leave(room);
+  });
+
+
+  // Insira o código que faz o trabalho do Signaling Server
+});
